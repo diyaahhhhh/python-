cfa68c877a00611ae2a3c3c067cea27b166f0e2a
diyaahhhhh <diyannjoseph2006@gmail.com>
2024-10-18 16:13:44 +0530

    Add files via upload

---
+++ mathmodule.py
@@ -0,0 +1,12 @@
+'''Author: Diya Ann Joseph
+Date: 18/10/24
+Description:a Python program that uses functions from the math module to perform the following operations on a number provided by the user:'''
+import math
+num=int(input("Enter a number:"))
+sq_root=math.sqrt(num)
+print("Square root:",sq_root)
+fact=math.factorial(num)
+print("Factorial:", fact)
+n=2
+POW=math.pow(num,n)
+print("Power:",POW)
