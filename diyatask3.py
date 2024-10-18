680822d468a02b318275d86d7a690056693b7950
diyaahhhhh <diyannjoseph2006@gmail.com>
2024-10-18 15:57:31 +0530

    Add files via upload

---
+++ diyatask3.py
@@ -0,0 +1,10 @@
+num1=int(input('Enter a number:'))
+num2=int(input('Enter another number:'))
+SUM=num1+num2
+print("Sum=",SUM)
+div=num1/num2
+print("Division=",div)
+print('Is num1 greater than num2?',num1>num2)
+print('Are num1 and num2 equal?',num1==num2)
+print("Logical AND:",num1>0 and num2>0)
+print("Logical OR:",num1>0 or num2>0)
