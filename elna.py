283ab3597af1c1d77d887601d3460efac19dcbc1
diyaahhhhh <diyannjoseph2006@gmail.com>
2024-10-18 14:35:26 +0530

    Add files via upload

---
+++ elna.py
@@ -0,0 +1,11 @@
+'''Author: Diya Ann Joseph
+Date:18/10/24
+Description:'a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+Enter your name: John
+Enter your age: 25
+Hello, John! You are 25 years old.'''
+
+n=input('Enter the name:')
+a=input('Enter your age:')
+print('Hello,',n+'!','You\'re',a,'years old.')
