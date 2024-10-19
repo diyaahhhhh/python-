91fbf281b3de97b63d794a14b2cbc555b23a14f7
diyaahhhhh <diyannjoseph2006@gmail.com>
2024-10-19 14:30:23 +0530

    Add files via upload

---
+++ 19_oct_task1.py
@@ -0,0 +1,16 @@
+'''Author: Diya Ann Joseph
+Date: 19/10/24
+Decsription: simple calculator'''
+num1=int(input("Enter a number:"))
+num2=int(input("Enter any number:"))
+num3=int(input("Enter another number:"))
+SUM=num1+num2
+print("The sum of num1 and num2 is:",SUM)
+diff=num2-num1
+print("The difference when num2 is subtracted from num1 is:",diff)
+pro=num1*num2
+print("THe product of num1 and num2 is:",pro)
+div=num1/num2
+print("The remainder when num1 is divided by num2 is:",div)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is:",result)
