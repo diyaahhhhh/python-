944b01162301bd8a406fb365014b204f35f41c14
diyaahhhhh <diyannjoseph2006@gmail.com>
2024-10-25 14:25:15 +0530

    Add files via upload

---
+++ 25_oct.py
@@ -0,0 +1,7 @@
+'''Description: To check if a number is greater or not'''
+num1=int(input('Enter a number:'))
+num2=int(input('Enter another number:'))
+if num1>num2:
+    print(num1,'is greater than',num2)
+else:
+    print(num2,'is greater than',num1)
