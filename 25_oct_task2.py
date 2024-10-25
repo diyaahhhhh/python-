9427b3a8fc903d54922c76d4815887bc7040027f
diyaahhhhh <diyannjoseph2006@gmail.com>
2024-10-25 14:28:06 +0530

    Add files via upload

---
+++ 25_oct_task2.py
@@ -0,0 +1,6 @@
+'''Description: To check if a number is positive or not'''
+num=int(input('Enter a number:'))
+if num>0:
+    print('The number is positive')
+else:
+    print('The number is negative')
