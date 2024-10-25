e756bd0c9dcdc7575d5fab38212d6c871f50b51f
diyaahhhhh <diyannjoseph2006@gmail.com>
2024-10-25 14:37:55 +0530

    Add files via upload

---
+++ 25_task3.py
@@ -0,0 +1,8 @@
+'''Description: To check if a number is positive,negative or equal to 0'''
+num=int(input('Enter a number:'))
+if num>0:
+    print('The number is positive')
+elif num==0:
+    print(num)
+else:
+    print('The number is negative')
